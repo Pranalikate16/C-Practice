ed331679e5ce05b4d4717f4d75ed0ce3e0f4365e
Pranali Kate <pranalikate722@gmail.com>
2026-09-21 17:02:17 +0530

    Delegatecontinued

---
+++ delegatecontinued.cs
@@ -0,0 +1,50 @@
+﻿
+public class Employee
+{ 
+public int Id { get; set; }
+    public string Name { get; set; }
+    public string Gender { get; set; }
+    public int Experience { get; set; }
+
+
+}
+
+public class Company
+{ 
+public string Name { get; set; }
+    private Employee[] _employees;
+
+    public Company(Employee[] employees)
+    {
+        _employees = employees;
+
+    }
+
+    public void AllEmployees()
+    {
+        Console.WriteLine($"Company Name : {Name}");
+        if (_employees != null && _employees.Length > 0)
+        {
+            foreach (Employee emp in _employees)
+            {
+                Console.WriteLine($"Id {emp.Id} name {emp.Name} gender {emp.Gender} experience {emp.Experience}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("No Employee Found In This Company");
+        }
+    }
+
+    public void PromoteEmployees()
+    {
+        foreach (Employee emp in _employees)
+        {
+            if (emp.Experience >= 3)
+            {
+                Console.WriteLine($"{emp.Name} is promoted");
+            }
+        }
+    }
+
+}
