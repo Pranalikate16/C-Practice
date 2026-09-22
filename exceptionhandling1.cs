f7a5a4635a78ebf8eb7928942cf8c5ab9de4fc39
Pranali Kate <pranalikate722@gmail.com>
2026-09-22 10:38:36 +0530

    ExceptionHandling

---
+++ exceptionhandling1.cs
@@ -0,0 +1,99 @@
+﻿
+#region first
+
+//Console.WriteLine("Welcome To Calculator App");
+
+//Console.WriteLine("Please Enter A Numerator");
+//int num = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("Please Enter A Denominator");
+//int den = int.Parse(Console.ReadLine());
+
+//try
+//{
+//    calculator c1 = new calculator();
+//    c1.Divide(num, den);
+
+//}
+//catch(Exception ex)
+//{
+//    Console.WriteLine("Outer catch block");
+//}
+//Console.WriteLine("Thank You Visit Again");
+
+#endregion first
+
+#region second
+
+//try
+//{
+//    Console.WriteLine("Welcome to calculator app");
+
+
+////    Console.WriteLine("Please enter a numerator");
+//    int num = int.Parse(Console.ReadLine());
+
+//    Console.WriteLine("Please enter a denominator");
+//    int den = int.Parse(Console.ReadLine());
+
+//    calculator c1 = new calculator();
+//    c1.Divide(num, den);
+//}
+//catch (FormatException ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
+//catch (OverflowException ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
+
+//Console.WriteLine("Thank You .Visit Again");
+
+#endregion second
+
+#region third
+
+//try
+//{
+//    Console.WriteLine("Welcome To Calculator App");
+
+//    Console.WriteLine("Please enter a numerator");
+
+//    int num;
+//    if (int.TryParse(Console.ReadLine(), out num))
+//    {
+//        Console.WriteLine("Please enter denominator");
+//        //  int den = int.Parse(Console.ReadLine());
+
+//        int den;
+//        if (int.TryParse(Console.ReadLine(), out den))
+//        {
+//            calculator c1 = new calculator();
+//            c1.Divide(num, den);
+//        }
+//        else
+//        {
+//            Console.WriteLine("Please enter valid denominator");
+//        }
+//    }
+//    else
+//    {
+//        Console.WriteLine("Please enter valid numerator");
+//    }
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine("outer catch block");
+//    Console.WriteLine(ex.Message);
+//    Console.WriteLine(ex.GetType().Name);
+
+//}
+
+//Console.WriteLine("Thank You . Visit Again");
+
+#endregion third
+
+
+
+Console.ReadLine();
